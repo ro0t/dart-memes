b8d3555d40f46296781aa33ad6a8fa34ba16b69d
ro0t <adam@igital.co>
2018-10-19 23:02:14 +0000

    Added animation for bullseye

--- src/assets/loader.ts
+++ src/assets/loader.ts
@@ -1,12 +1,19 @@
+/**
+ * Figure out a better way to load resources
+ * Has to be a better way to bundle assets with Parcel
+ */
 // @ts-ignore
 import trump1 from './trump/trump1.png';
 // @ts-ignore
 import trump2 from './trump/trump2.png';
 // @ts-ignore
 import trump3 from './trump/trump3.png';
+// @ts-ignore
+import bullseye from './bullseye/bullzii.png';
 
 export const TRUMP_ONE = trump1;
 export const TRUMP_TWO = trump2;
 export const TRUMP_THREE = trump3;
+export const BULLSEYE = bullseye;
 
 export default { };
--- src/components/Memes.tsx
+++ src/components/Memes.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { TRUMP_ONE, TRUMP_TWO, TRUMP_THREE } from '../assets/loader';
+import { TRUMP_ONE, TRUMP_TWO, TRUMP_THREE, BULLSEYE } from '../assets/loader';
 
 const TRUMPS: any[] = [TRUMP_ONE, TRUMP_TWO, TRUMP_THREE];
 const TRUMP_QUOTES: string[] = ['MAKE \'MURICA GR8 AGAIN', 'DONALD.. TRUMPED!', 'LIGHTWEIGHT'];
 const TRUMP_ANIM_SECONDS: number = 4;
+
+const BULLS_I_MESSAGE: string = "BULLS 👁";
+const BULLS_I_ANIM_SECONDS: number = 2;
+
+/** The overlay that all animations appear in and such. */
+const GameOverlayContainer = styled.div`
+    position: absolute;
+    top:0;
+    right:0;
+    left:0;
+    bottom:0;
+    pointer-events: none;
+    transform: translate3d(0,0,0);
+    
+    /** Shared animation */
+    @keyframes datMessageRiteThurr {
+        0% { transform: translate3d(0,0,0) scale(0); }
+        75% { transform: translate3d(0,0,0) scale(1.2); }
+        100% { transform: translate3d(0,0,0) scale(1); }
+    }
+`;
 
 interface ITrump {
     currentTrump: number;
@@ -21,47 +42,23 @@ const Trump = styled.div<ITrump>`
     position: absolute;
     
     @keyframes showTrump {
-        from {
-            transform: translate3d(100%, 100%, 0) scale(0.2);
-        }
-        
-        to {
-            transform: translate3d(0, 0, 0) scale(1);
-        }
+        from { transform: translate3d(100%, 100%, 0) scale(0.2); }
+        to { transform: translate3d(0, 0, 0) scale(1); }
     } 
 `;
 
-const TrumpContainer = styled.div`
-    position: absolute;
-    top:0;
-    right:0;
-    left:0;
-    bottom:0;
-    pointer-events: none;
-`;
-
-const TrumpMessage = styled.h1`
+const BigAssMessage = styled.h1`
     font-size: 3.5rem;
     top: 25%;
     position: absolute;
     width: 100%;
     text-align: center;
+    z-index: 1000;
+`;
+
+const TrumpMessage = styled(BigAssMessage)`
     color: darkorange;
     animation: datMessageRiteThurr .6s ease-in forwards, datShadowRiteThurr .1s infinite forwards;
-    
-    @keyframes datMessageRiteThurr {
-        0% {
-            transform: translate3d(0,0,0) scale(0);
-        }
-        
-        75% {
-            transform: translate3d(0,0,0) scale(1.2);
-        }
-        
-        100% {
-            transform: translate3d(0,0,0) scale(1);
-        }
-    }
     
     @keyframes datShadowRiteThurr {
         from {
@@ -74,6 +71,39 @@ const TrumpMessage = styled.h1`
     }      
 `;
 
+const Bullseye = styled.div`
+    --bullseye-size: 200px;
+    background: url(${BULLSEYE}) center center no-repeat;
+    background-size: 100%;
+    width: var(--bullseye-size);
+    height: var(--bullseye-size);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    animation: pulseBullsEye .2s forwards infinite;
+    z-index: 900;
+    
+    @keyframes pulseBullsEye {
+        0% { transform: translate3d(-50%, -50%, 0) scale(1); }
+        50% { transform: translate3d(-50%, -50%, 0) scale(1.25); }
+        100% { transform: translate3d(-50%, -50%, 0) scale(1); }
+    }
+`;
+
+const BullseyeMessage = styled(BigAssMessage)`
+    color: #fff;
+    animation: datMessageRiteThurr .6s ease-in forwards, datShadowRiteThurr .1s infinite forwards;
+    
+    @keyframes datShadowRiteThurr {
+        from {
+            text-shadow: red 4px 4px 0, darkred -4px -4px 0;
+        }
+        
+        to {
+            text-shadow: darkred -4px -4px 0, red 4px 4px 0;
+        }
+    }      
+`;
 
 const TrumpIt = styled.button`
     padding: 12px 16px;
@@ -97,6 +127,15 @@ const TrumpIt = styled.button`
     }
 `;
 
+const BullsIt = styled(TrumpIt)`
+    color: red;
+    margin-left: 12px;
+    
+    &:hover {
+        background: red;
+    }
+`;
+
 class Memes extends React.Component<any, any> {
     private readonly TOTAL_TRUMPS: number = TRUMPS.length;
 
@@ -105,6 +144,7 @@ class Memes extends React.Component<any, any> {
 
         this.state = {
             showTrump: false,
+            showBullseye: false,
             currentTrump: 1
         };
     }
@@ -129,19 +169,34 @@ class Memes extends React.Component<any, any> {
         }
     };
 
-    render() {
+    bullseye = () => {
+        if(!this.state.showBullseye) {
+            this.setState({ ...this.state, showBullseye: true });
 
+            // Hide that trump fuq
+            setTimeout(() => {
+                this.setState({ ...this.state, showBullseye: false });
+            }, BULLS_I_ANIM_SECONDS * 1000);
+        }
+    };
+
+    render() {
         const trumpIndex = this.state.currentTrump - 1;
         const TRUMP_ASSET = TRUMPS[trumpIndex];
         const TRUMP_MESSAGE = TRUMP_QUOTES[trumpIndex];
 
         return (
             <React.Fragment>
-                <TrumpIt onClick={this.trump}>Trump it</TrumpIt>
-                <TrumpContainer>
+                <div className={'dev-buttons-container'}>
+                    <TrumpIt onClick={this.trump}>Trump it</TrumpIt>
+                    <BullsIt onClick={this.bullseye}>Bullseye</BullsIt>
+                </div>
+                <GameOverlayContainer>
                     {this.state.showTrump && <TrumpMessage>{TRUMP_MESSAGE}</TrumpMessage>}
                     {this.state.showTrump && <Trump style={{ backgroundImage: `url(${TRUMP_ASSET})` }} />}
-                </TrumpContainer>
+                    {this.state.showBullseye && <BullseyeMessage>{BULLS_I_MESSAGE}</BullseyeMessage>}
+                    {this.state.showBullseye && <Bullseye />}
+                </GameOverlayContainer>
             </React.Fragment>
         );
     }
